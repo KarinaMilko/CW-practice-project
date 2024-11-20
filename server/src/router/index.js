82faef5d2d9cab74beb6d9dde3adc4d1d2cc204e
Karina <milkokarina13@gmail.com>
2024-11-20 20:00:37 +0200

    Refactor get /getContestById

--- server/src/router/index.js
+++ server/src/router/index.js
@@ -37,11 +37,15 @@ router.post(
   contestController.dataForContest
 );
 
-router.post(
-  '/getCustomersContests',
-  checkToken.checkToken,
-  contestController.getCustomersContests
-);
+// get /contests/customers
+// /users/id/contests
+// /customers/id/contests
+// get /contests/byCustomer
+// router.post(
+//   '/getCustomersContests',
+//   checkToken.checkToken,
+//   contestController.getCustomersContests
+// );
 
 // get /getContestById -> get /contests/:id
 // headers -> params
