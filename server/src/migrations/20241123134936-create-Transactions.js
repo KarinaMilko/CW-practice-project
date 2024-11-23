00e40c8ae8e75ea8ad1dafcee22e3eae49fdac73
Karina <milkokarina13@gmail.com>
2024-11-23 16:21:47 +0200

    Refactor transaction migration

--- server/src/migrations/20241123134936-create-Transactions.js
+++ server/src/migrations/20241123134936-create-Transactions.js
@@ -1,48 +1,51 @@
 'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Transactions', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      amount: {
-        type: Sequelize.DECIMAL,
-        allowNull: false,
-      },
-      operationType: {
-        type: Sequelize.ENUM(['INCOME', 'EXPENSE']),
-        allowNull: false,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'Users',
-          key: 'id',
+  up(queryInterface, Sequelize) {
+    return queryInterface
+      .createTable('Transactions', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
         },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE',
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-    await queryInterface.addConstraint('Transactions', {
-      fields: ['amount'],
-      type: 'check',
-      where: { amount: { [Sequelize.Op.gt]: 0 } },
-    });
+        amount: {
+          type: Sequelize.DECIMAL,
+          allowNull: false,
+        },
+        operationType: {
+          type: Sequelize.ENUM(['INCOME', 'EXPENSE']),
+          allowNull: false,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+          onDelete: 'CASCADE',
+          onUpdate: 'CASCADE',
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      })
+      .then(() =>
+        queryInterface.addConstraint('Transactions', {
+          fields: ['amount'],
+          type: 'check',
+          where: { amount: { [Sequelize.Op.gt]: 0 } },
+        })
+      );
   },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Transactions');
+  down(queryInterface, Sequelize) {
+    return queryInterface.dropTable('Transactions');
   },
 };
